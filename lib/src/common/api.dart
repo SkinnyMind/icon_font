a94f6c0cfc99cc604183c8edb77bd63ccfa016c1
Aleksey Kulikov <skinny.mind@gmail.com>
2025-01-20 21:54:43 +0300

    refactor: stop depending on `recase` (#46)

--- lib/src/common/api.dart
+++ lib/src/common/api.dart
@@ -5,7 +5,6 @@ import 'package:icon_font/src/utils/konst.dart';
 import 'package:icon_font/src/utils/logger.dart';
 import 'package:logger/logger.dart';
 import 'package:path/path.dart' as p;
-import 'package:recase/recase.dart';
 
 class IconFont {
   const IconFont._();
@@ -232,4 +231,66 @@ class SvgToOtfResult {
 
   final List<GenericGlyph> glyphList;
   final OpenTypeFont font;
+}
+
+class ReCase {
+  ReCase(String text) {
+    originalText = text;
+    _words = _groupIntoWords(text);
+  }
+
+  final _upperAlphaRegex = RegExp('[A-Z]');
+
+  final symbolSet = {' ', '.', '/', '_', r'\', '-'};
+
+  late String originalText;
+  late List<String> _words;
+
+  List<String> _groupIntoWords(String text) {
+    final sb = StringBuffer();
+    final words = <String>[];
+    final isAllCaps = text.toUpperCase() == text;
+
+    for (var i = 0; i < text.length; i++) {
+      final char = text[i];
+      final nextChar = i + 1 == text.length ? null : text[i + 1];
+
+      if (symbolSet.contains(char)) {
+        continue;
+      }
+
+      sb.write(char);
+
+      final isEndOfWord = nextChar == null ||
+          (_upperAlphaRegex.hasMatch(nextChar) && !isAllCaps) ||
+          symbolSet.contains(nextChar);
+
+      if (isEndOfWord) {
+        words.add(sb.toString());
+        sb.clear();
+      }
+    }
+
+    return words;
+  }
+
+  String get camelCase => _getCamelCase();
+
+  String _getCamelCase({String separator = ''}) {
+    final words = _words.map(_upperCaseFirstLetter).toList();
+    if (_words.isNotEmpty) {
+      words[0] = words[0].toLowerCase();
+    }
+
+    return words.join(separator);
+  }
+
+  String _upperCaseFirstLetter(String word) {
+    return '${word.substring(0, 1).toUpperCase()}'
+        '${word.substring(1).toLowerCase()}';
+  }
+}
+
+extension StringReCase on String {
+  String get camelCase => ReCase(this).camelCase;
 }
